bf79a8a9dbe5c5878c292247ef04b277e6363635
AffrizaWildanFauzan <5052241033@student.its.ac.id>
2026-04-17 08:16:19 -0700

    Update statistika_deskriptif_scratch.R

--- script/statistika_deskriptif_scratch.R
+++ script/statistika_deskriptif_scratch.R
@@ -1,118 +1,136 @@
-
 # 1. Load Data
-data <- read.csv("C:/Users/Constantine Calvin/OneDrive/Constantine's Work/dataset_walmart.csv")
+set.seed(42)
+df<-read.csv("~/Downloads/dataset_walmart.csv")
+# 2. Cek Struktur & Data Awal
+str(df)
+head(df)
+dim(df)
+View(df)
+# 3. Statistik Deskriptif Numerik
+# summary dasar
+summary(df)
 
-x <- data$Weekly_Sales
-
-# 2. Hapus NA
-x <- x[!is.na(x)]
-n <- length(x)
-
-# 3. Mean
-total <- 0
-for (i in 1:n) {
-  total <- total + x[i]
-}
-mean <- total / n
-
-# 4. Sorting (Bubble Sort)
-x_sorted <- x
-for (i in 1:(n-1)) {
-  for (j in 1:(n-i)) {
-    if (x_sorted[j] > x_sorted[j+1]) {
-      temp <- x_sorted[j]
-      x_sorted[j] <- x_sorted[j+1]
-      x_sorted[j+1] <- temp
-    }
+colSums(is.na(df))
+# fungsi statistik lengkap
+mean<-function(x){
+  sum=0
+  n<-length(x)
+  for(i in 1:n){
+    sum=sum+x[i]
   }
+  xbar=sum/n
+  return(xbar)
 }
 
-# 5. Median
-if (n %% 2 == 1) {
-  median <- x_sorted[(n+1)/2]
-} else {
-  median <- (x_sorted[n/2] + x_sorted[n/2 + 1]) / 2
-}
-
-# 6. Q1 dan Q3 (Linear Interpolation)
-pos_q1 <- (n + 1) / 4
-pos_q3 <- 3 * (n + 1) / 4
-
-# Q1
-lower <- floor(pos_q1)
-upper <- ceiling(pos_q1)
-if (lower == upper) {
-  Q1 <- x_sorted[lower]
-} else {
-  Q1 <- x_sorted[lower] + (pos_q1 - lower) * (x_sorted[upper] - x_sorted[lower])
-}
-
-# Q3
-lower <- floor(pos_q3)
-upper <- ceiling(pos_q3)
-if (lower == upper) {
-  Q3 <- x_sorted[lower]
-} else {
-  Q3 <- x_sorted[lower] + (pos_q3 - lower) * (x_sorted[upper] - x_sorted[lower])
-}
-
-# 7. IQR
-IQR <- Q3 - Q1
-
-# 8. Variance
-total_var <- 0
-for (i in 1:n) {
-  total_var <- total_var + (x[i] - mean)^2
-}
-variance <- total_var / (n - 1)
-
-# 9. Standard Deviation
-sd <- variance^(1/2)
-
-# 10. Minimum
-min <- x[1]
-for (i in 2:n) {
-  if (x[i] < min) {
-    min <- x[i]
+stadev<-function(x){
+  xbar<-mean(x)
+  sum=0
+  n<-length(x)
+  for(i in 1:n){
+    sum=sum+(x[i]-xbar)^2
   }
+  std=sqrt(sum)/n
+  return(std)
 }
 
-# 11. Maximum
-max <- x[1]
-for (i in 2:n) {
-  if (x[i] > max) {
-    max <- x[i]
+varians<-function(x){
+  xbar<-mean(x)
+  sum=0
+  n<-length(x)
+  for(i in 1:n){
+    sum=sum+(x[i]-xbar)^2
   }
+  var=sum/n
+  return(var)
 }
 
-# 12. Range
-range <- max - min
-
-# 13. Skewness
-total_skew <- 0
-for (i in 1:n) {
-  total_skew <- total_skew + (x[i] - mean)^3
+kovarians<-function(x,y){
+  xbar<-mean(x)
+  ybar<-mean(y)
+  n<-length(x)
+  sum=0
+  for(i in 1:n){
+    sum=sum+((x[i]-xbar)*(y[i]-ybar))
+  }
+  kov=sum/n
+  return(kov)
 }
-skewness <- (total_skew / n) / (sd^3)
 
-# 14. Kurtosis
-total_kurt <- 0
-for (i in 1:n) {
-  total_kurt <- total_kurt + (x[i] - mean)^4
+korelasi<-function(x,y){
+  xbar<-mean(x)
+  ybar<-mean(y)
+  n<-length(x)
+  atas=0
+  bawah_1=0
+  bawah_2=0
+  for(i in 1:n){
+    atas=atas+((x[i]-xbar)*(y[i]-ybar))
+    bawah_1=bawah_1+(x[i]-xbar)^2
+    bawah_2=bawah_2+(y[i]-ybar)^2
+  }
+  bawah_1_final=sqrt(bawah_1)
+  bawah_2_final=sqrt(bawah_2)
+  kor=atas/(bawah_1_final*bawah_2_final)
+  return(kor)
 }
-kurtosis <- (total_kurt / n) / (sd^4)
 
-# 15. Output
-mean
-median
-Q1
-Q3
-IQR
-variance
-sd
-min
-max
-range
-skewness
-kurtosis
+median<-function(x){
+  n<-length(x)
+  if(n%%2==0){
+    med=x[n/2]+x[(n+2)/2]
+  }
+  if(n%%2!=0){
+    med=x[(n+1)/2]
+  }
+  return(med)
+}
+
+modus<-function(x){
+  uniq_val<-unique(x)
+  uniq_val[which.max(tabulate(match(x,uniq_val)))]
+}
+
+med_x<-median(df$Weekly_Sales)
+med_x
+
+med_y<-median(df$Temperature)
+med_y
+
+std_x<-stadev(df$Weekly_Sales)
+std_x
+
+std_y<-stadev(df$Temperature)
+std_y
+
+var_x<-varians(df$Weekly_Sales)
+var_x
+
+var_y<-varians(df$Temperature)
+var_y
+
+kov_xy<-kovarians(df$Weekly_Sales,df$Temperature)
+kov_xy
+
+kor_xy<-korelasi(df$Weekly_Sales,df$Temperature)
+kor_xy
+
+mod_x<-modus(df$Weekly_Sales)
+mod_x
+
+mod_y<-modus(df$Temperature)
+mod_y
+
+hasil_statdes<-data.frame(
+  varians_x=var_x,
+  varians_y=var_y,
+  kovarians=kov_xy,
+  korelasi=kor_xy,
+  standard_deviation_x=std_x,
+  standard_deviation_y=std_y,
+  median_x=med_x,
+  median_y=med_y,
+  modus_x=mod_x,
+  modus_y=mod_y
+)
+hasil_statdes
 
